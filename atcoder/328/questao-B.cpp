4dd20478aac8ad96fa66a1b092cb38af500747e4
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-11-16 17:22:14 -0300

    questão B do abc328

--- atcoder/328/questao-B.cpp
+++ atcoder/328/questao-B.cpp
@@ -8,9 +8,11 @@ int algiguais(int n){
     }
     if (n < 100){
         int m = n%10;
-        n = n/10;
+        n = n/10; 
         if (m == n) return n;
+        else return -1;
     }
+   
     return -1;
 }
 
@@ -23,11 +25,10 @@ int main(){
         dias[i] = d;
     }
     int ans = 0;
-    int x = 0;
+    int x = 1;
     while (x <= meses){
-        for(int i = 0; i < dias[x]; i++){
+        for(int i = 1; i < dias[x-1]; i++){
             if (algiguais(i) == algiguais(x) && algiguais(i) > 0 && algiguais(x) > 0){
-                cout << i << " " << x << endl;
                 ans++;
             }
         }
