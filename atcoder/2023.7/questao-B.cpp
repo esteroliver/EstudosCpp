69060c9fff7622714b6a48588836fa487622f7b4
esteroliver <20231014040002@ifrn.local>
2023-11-16 14:37:50 -0300

    questão B atcoder

--- atcoder/2023.7/questao-B.cpp
+++ atcoder/2023.7/questao-B.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+int algiguais(int n){
+    if (n < 10){
+        return n;
+    }
+    if (n < 100){
+        int m = n%10;
+        n = n/10;
+        if (m == n) return n;
+    }
+    return -1;
+}
+
 int main(){
     int meses; cin >> meses;
     int dias[meses];
@@ -10,6 +22,17 @@ int main(){
         cin >> d;
         dias[i] = d;
     }
-    
+    int ans = 0;
+    int x = 0;
+    while (x <= meses){
+        for(int i = 0; i < dias[x]; i++){
+            if (algiguais(i) == algiguais(x) && algiguais(i) > 0 && algiguais(x) > 0){
+                cout << i << " " << x << endl;
+                ans++;
+            }
+        }
+        x++;
+    }
+    cout << ans << endl;
     return 0;
 }
