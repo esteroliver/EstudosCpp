372c99ce8738001826e9411448512f7de5068c3c
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-12-14 14:27:04 -0300

    exercício de grafo

---
+++ grafos/questao-A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dfs(vector<vector<int>> grafo , vector<bool> visitados, int i){
+    visitados[i] = true;
+    for (int x : grafo[i]){
+        if (!visitados[x]) dfs(grafo, visitados, x);
+    }
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    int cs = stoi(s);
+    getline(cin,s);
+    
+    for(int i = 0; i < cs; i++){
+        getline(cin,s);
+        char in_c = s[0];
+        int quant = in_c - 'A' + 1;
+        vector<vector<int>> g(quant);
+        while(getline(cin,s), s != ""){
+            int v=s[0] - 'A';
+            int u=s[1] - 'A';
+            g[v].push_back(u);
+            g[v].push_back(u);
+        }
+    }
+
+    return 0;
+}
