9739e2277492de574330ad2732b4311c08a070aa
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-12-28 14:23:31 -0300

    Questão B atcoder 334 (wrong answer)

--- atcoder/334/questao-B.cpp
+++ atcoder/334/questao-B.cpp
@@ -7,23 +7,58 @@ int main(){
   cin >> a >> m >> l >> r;
 
   long long arvores = 0;
-  
-  if (a <= l){
+
+  if(l == r){
+    if(a <= l){
+        long long esp_a = abs(l - a);
+        esp_a = esp_a/m;
+        a += m * esp_a;
+        if (a < l){ 
+            a+=m;
+        }
+        if(a == l) arvores = 1;
+    }
+    else if(a >= r){
+        long long esp_a = abs(a - r);
+        esp_a = esp_a/m;
+        a -= m * esp_a;
+        if (a > r){
+            a-=m;
+        }
+        if (a == r) arvores = 1;
+    }
+    else{
+        arvores = 0;
+    }
+  }
+
+  else if (a <= l){
     long long esp_a = abs(l - a);
     esp_a = esp_a/m;
     a += m * esp_a;
-    if (a < l) a+=m;
-    arvores = abs(r - a)/m + 1;
+    if (a < l){ 
+        a+=m;
+        arvores++;
+    }
+    else if(a == l) arvores++;
+    arvores += abs(r - a)/m;
   }
-  if(a >= r){
+
+  else if(a >= r){
     long long esp_a = abs(a - r);
     esp_a = esp_a/m;
     a -= m * esp_a;
-    if (a > r) a-=m;
-    arvores = abs(a - l)/m;
+    if (a > r){
+        a-=m;
+        arvores++;
+    }
+    else if (a == r) arvores++;
+    arvores += abs(a - l)/m;
   }
-  if (a > l && a < r){
+
+  else if (a > l && a < r){
     arvores = abs(a - l)/m + abs(r - a)/m + 1;
   }
+  
   cout << arvores << endl;
 }
