d132ff18516a02ed22e89b16bdc73aa6fc11319b
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2024-01-03 13:45:59 -0300

    task 1192

---
+++ grafos/task-1192.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int quartos_cont(char quartos[1000][1000], int l, int c){
+    for(int i = 0; i < c; i++){
+        
+    }
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    char quartos[n][m];
+    for(int i = 0; i < n; i++){
+        string x;
+        cin >> x;
+        for(int j = 0; j < n; j++){
+            quartos[i][j] = x[j];
+        }
+    }
+}
