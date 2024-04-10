90a4df2b0cf7c525499450a0ac8069963fedc2d6
esteroliver <oliver.ester.2004@gmail.com>
2024-04-10 20:12:40 -0300

    refazendo a questão 2253

--- beecrowd/2253.cpp
+++ beecrowd/2253.cpp
@@ -14,10 +14,14 @@ int main(){
         bool carac = false;
         bool tam = false;
         for(int i = 0; i < senha.size(); i++){
-            if((senha[i] >= 32 && senha[i] <= 47) || (senha[i] >= 58 && senha[i] <= 64) || (senha[i] >= 91 && senha[i] <= 96)) carac = true;
-            if(senha[i] >= 65 && senha[i] <= 90) max = true;
-            if(senha[i] >= 97 && senha[i] <= 122) min = true;
-            if(senha[i] >= 48 && senha[i] <= 57) num = true;
+            if((senha[i] >= 'A' && senha[i] <= 'Z') || (senha[i] >= 'a' && senha[i] <= 'z') || (senha[i] >= '0' && senha[i] <= '9')){
+                if(senha[i] >= 'A' && senha[i] <= 'Z') max = true;
+                if(senha[i] >= 'a' && senha[i] <= 'z') min = true;
+                if(senha[i] >= '0' && senha[i] <= '9') num = true;
+            }
+            else{
+                carac = true;
+            }
         }
         if(senha.size() >= 6 && senha.size() <= 32) tam = true;
         if(max && min && num && tam && !carac) valida = "Senha valida.";
