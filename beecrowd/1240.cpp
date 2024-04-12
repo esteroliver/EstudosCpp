df3dd2cf2b84a0958fa168a939fc1f9bb591823e
esteroliver <oliver.ester.2004@gmail.com>
2024-04-12 19:32:44 -0300

    guarda costeira

--- beecrowd/1240.cpp
+++ beecrowd/1240.cpp
@@ -6,16 +6,38 @@ int main(){
     int n; 
     cin >> n;
     for(int i = 0; i < n; i++){
-        int a, b;
+        long long a, b;
         cin >> a >> b;
         string resp;
-        if(a > 99 && b > 99){
-            int r1 = (a/100) * 100;
-            r1 = a%r1;
-            int r2 = (b/100) * 100;
-            r2 = b%r2;
-            if (r1 == r2) resp = "encaixa";
-            else resp = "nao encaixa";
+        if(a >= b){
+            if(a > 99){
+                long long  r1 = (a/100) * 100;
+                r1 = a%r1;
+                long long r2;
+                if(b > 99){
+                    r2 = (b/100) * 100;
+                    r2 = b%r2;
+                }
+                else r2 = b;
+                if (r1 == r2) resp = "encaixa";
+                else resp = "nao encaixa";
+            }
+            else if (a > 9){
+                long long  r1 = (a/10) * 10;
+                r1 = a%r1;
+                long long  r2;
+                if(b > 9){
+                    r2 = (b/10) * 10;
+                    r2 = b%r2;
+                }
+                else r2 = b;
+                if(r1 == r2) resp = "encaixa";
+                else resp = "nao encaixa";
+            }
+            else{
+                if (a == b) resp = "encaixa";
+                else resp = "nao encaixa";
+            }
         }
         else resp = "nao encaixa";
         cout << resp << endl;
--- beecrowd/1247.cpp
+++ beecrowd/1247.cpp
@@ -3,22 +3,18 @@
 using namespace std;
 
 int main(){
-    int x;
-    while(cin >> x){
+    int d;
+    while(cin >> d){
         //limite das águas - 12 milhas
-        int d, vf, vg; //distancia entre fugitivo e guarda, velocidade do fugitivo, velocidade da guarda
-        cin >> d >> vf >> vg;
-        int vfi = vf;
-        int vgi = vg;
+        int vf, vg; //distancia entre fugitivo e guarda, velocidade do fugitivo, velocidade da guarda
+        cin >> vf >> vg;
         string resp = "N";
-        for(int i = d; i < 12; i++){
-            vg += vgi;
-            vf += vfi;
-            if(vg > vf){
-                resp = "S";
-                break;
-            }
+        int r;
+        if(vg > vf){
+            r = vg - vf;
+            r = d/r;
         }
+        if(r < (12 - d)) resp = "S";
         cout << resp << endl;
     }
     return 0;
